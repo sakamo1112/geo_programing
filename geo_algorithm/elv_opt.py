268f6eccabcc70a18a5a1593a17affa007a476ab
坂本海人 <sakamo78911@gmail.com>
2024-01-03 01:46:30 +0900

    minsum完成

--- geo_algorithm/elv_opt.py
+++ geo_algorithm/elv_opt.py
@@ -77,16 +77,21 @@ def seek_wait_minsum(trans_img, floor_num, trans_data, test_num: int):
             user_data = [data for data in trans_data[:i] if data[2] == "user"]
             stop_fl = int(np.median([data[0] for data in user_data[-20:]]))
             stop_fl_list.append((i, stop_fl))
+    elv_move_nominsum_data, elv_move_nominsum_img = create_wait_data(trans_data, trans_img)
+    cv2.imwrite("nominsum.png", elv_move_nominsum_img)
             
     # 中継の停車階までの移動を画像に反映(停車階は番号の前に挿入するind:11なら10の位置に挿入)
     trans_data, trans_img = create_mid_stop_data(trans_data, trans_img, stop_fl_list)
 
     # 中継の停車階から次の始点までの移動を画像に反映(待ちの移動を赤色にする)
-    elv_move_data, elv_move_img = create_wait_data(trans_data, trans_img)
+    elv_move_minsum_data, elv_move_minsum_img = create_wait_data(trans_data, trans_img)
+    cv2.imwrite("minsum.png", elv_move_minsum_img)
 
+    # 待ち時間の平均を求める
 
-    cv2.imwrite("trans1.png", elv_move_img)
-    return elv_move_data
+    # 待ち時間の分散を求める
+
+    return elv_move_minsum_data
 
 def seek_0wait_maximize():
     pass
@@ -115,7 +120,6 @@ if __name__ == "__main__":
             trans_img[floor_num - (white_fl[0] - fl_j)][i] = [255, 255, 255]
         trans_img[floor_num - trans[0]][i] = [0, 255, 0]  # 始点: 緑
         trans_img[floor_num - trans[1]][i] = [255, 0, 230]  # 終点: ピンク
-    elv_move_data = seek_wait_minsum(trans_img, floor_num, trans_data, test_num)
-    print(elv_move_data)
-
     cv2.imwrite("trans.png", trans_img)
+
+    elv_move_data = seek_wait_minsum(trans_img, floor_num, trans_data, test_num)
